c0c93b3813fc4d29fe45ed4c19022965a3fdec98
Megan Fontenot <meganfontenot517@yahoo.com>
2020-01-08 20:19:19 +0100

    starting Prime Reduction

---
+++ primeReduction.js
@@ -0,0 +1,12 @@
+// in: 2 integers (which represent a range of numbers)
+// out: integer
+// build a function that checks if the number is prime
+// make an array to store data
+// create another function that will reduce values along the chain
+// ^ until we hit 1
+
+// use a for loop that runs through the range
+//   check if the number is prime
+//   reduce through the fancy chain property
+//     increment a counter
+// return the value of that counter
