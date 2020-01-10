8385ceb8bada3220f6bee1ff0079480ac35cda28
Megan Fontenot <meganfontenot517@yahoo.com>
2020-01-10 20:19:19 +0100

    finished

--- primeReduction.js
+++ primeReduction.js
@@ -50,6 +50,52 @@ function primeReduction(a, b) {
 
 primeReduction(8, 125);
 
+//  SOLUTION  //
+function primeReduction(a, b) {
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  const arr = [];
+  function reduction(n) {
+    if (arr.includes(n)) {
+      arr.length = 0;
+      return false;
+    }
+    arr.push(n);
+    if (n === 1) {
+      arr.length = 0;
+      return true;
+    }
+    let hold = 0;
+    let num = n;
+    while (num > 0) {
+      hold += Math.pow(num % 10, 2);
+      // hold = hold + Math.pow(5 % 10, 2);
+      // => 5
+      num -= num % 10;
+      // 5 = 5 - 5
+      // => 0
+      num /= 10;
+      // => 0
+    }
+    return reduction(hold);
+  }
+
+  let count = 0;
+  for (let i = a; i < b; i++) {
+    if (isPrime(i)) {
+      if (reduction(i)) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
 // in: 2 integers (which represent a range of numbers)
 // out: integer
 // build a function that checks if the number is prime
