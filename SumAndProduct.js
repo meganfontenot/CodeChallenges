55e19aee981e12fd8ae848fe88c20dd0ac765335
Megan Fontenot <meganfontenot517@yahoo.com>
2020-01-15 20:19:19 +0100

    solution

--- SumAndProduct.js
+++ SumAndProduct.js
@@ -15,3 +15,11 @@ function sumAndProduct(sum, product) {
     return null;
   }
   console.log(sumAndProduct(6, 9));
+
+  //  SOLUTION  //
+function sumAndProduct(sum, product) {
+	for (let i = 0; i < sum / 2; i++) {
+		if (i * (sum - i) === product) return [i, (sum - i)];
+	}
+	return null;
+}
