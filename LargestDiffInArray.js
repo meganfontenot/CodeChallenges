0d1eb4c58b1391ff8298959e425cfbd10627067c
Megan Fontenot <meganfontenot517@yahoo.com>
2020-01-18 20:19:19 +0100

    working fuction

--- LargestDiffInArray.js
+++ LargestDiffInArray.js
@@ -3,3 +3,18 @@
 // For example, [10, 7, 5, 8, 11, 9] the largestDifference is 6, from 5 to 11.
 
 // Given an array of numbers that don't return a positive number, such as [100, 90, 80, 50, 10] return 0. 
+
+function largestDifference(arr) {
+    let largest = 0;
+    for (let i = 0; i < arr.length; i++) {
+      for (let a = i + 1; a < arr.length; a++) {
+        if ((arr[a]-arr[i]) > largest) {
+          largest = arr[a]-arr[i];
+        }
+      }
+    }
+    return largest;
+  }
+  
+  largestDifference(100, 90, 80, 50, 10);
+  
