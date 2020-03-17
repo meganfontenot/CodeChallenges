aeac58ef9c75c2550d089f72f33ef9b501561605
Megan Fontenot <meganfontenot517@yahoo.com>
2020-03-17 13:04:49 -0500

    last update

--- forlooptimeout.js
+++ forlooptimeout.js
@@ -1,4 +1,4 @@
-// ES5 //
+// ES5 
 for (var i = 1; i <= 10; i++) {
 	(function(i){setTimeout(function() {
 		// From looking at the code you would assume it would print 1 - 10
@@ -8,7 +8,7 @@ for (var i = 1; i <= 10; i++) {
 }
 
 
-// ES6 //
+// ES6 
 for (let i = 1; i <= 10; i++) {
 	setTimeout(function() {
 		// From looking at the code you would assume it would print 1 - 10
