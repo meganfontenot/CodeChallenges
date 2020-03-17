552cc211d14ee2a472d3a76d19417e1e00ba69cc
Megan Fontenot <meganfontenot517@yahoo.com>
2020-03-17 13:04:30 -0500

    removed commented code

--- forlooptimeout.js
+++ forlooptimeout.js
@@ -7,13 +7,6 @@ for (var i = 1; i <= 10; i++) {
 	}, 0)})(i);
 }
 
-/*  setTimeout with a argument of zero doesn't actually mean zero seconds of delay.
-According to MDN it means "as soon as possible".  It is still a few milliseconds of delay.
-So, the function waits for those few milliseconds and in the meantime the loop finishes
-with the value of i ending up at 11.  When each of the functions finally run, i is 11.
-So the code below works where the function gets a new scope each time through and each
-time i is different.  Or you can just change var to let...
-*/
 
 // ES6 //
 for (let i = 1; i <= 10; i++) {
