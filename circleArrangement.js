7ad9efda2cc3c1d1344d33912caa1f7f161686ee
Megan Fontenot <meganfontenot517@yahoo.com>
2020-03-25 22:13:02 -0500

    sudo code

---
+++ circleArrangement.js
@@ -0,0 +1,21 @@
+const circle = document.getElementById('circle');
+
+const arrange = (diameter=150, numElements=10, childSize=25) => {
+  //Set height and width of the containing circle
+  //Get the radius of the parent less the childSize
+  //Create a spacial step that will offset the children
+  //circles evenly based the the number of them
+  //Declare an incementable angle that will space the 
+  //proceding children based on the step
+  //Loop through the input number of children elements
+    //Find the x and y coordinates of their position in the circle
+    //based on the diameter of the parent and the current location
+    //of the angle
+    //Create a child element and assign it the class sub-circle
+    //This will apply styles that will make it circular
+    //Append the child to the parent circle
+    //Set the top, left, height and width values of the child
+    //increment the angle based on the step
+}
+
+//Helper function for setting styles
