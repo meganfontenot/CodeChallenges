45f5cac142ba12b2d9e785a441882ec43f2a0696
Megan Fontenot <meganfontenot517@yahoo.com>
2020-04-26 11:49:07 -0500

    Update forlooptimeout.js

--- OldChallenges/forlooptimeout.js
+++ OldChallenges/forlooptimeout.js
@@ -2,7 +2,6 @@
 for (var i = 1; i <= 10; i++) {
 	(function(i){setTimeout(function() {
 		// From looking at the code you would assume it would print 1 - 10
-		// It doesn't.  Why?  How can you make it print 1 - 10.
 		console.log(i);
 	}, 0)})(i);
 }
