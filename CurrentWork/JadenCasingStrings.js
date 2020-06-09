af3aa76b9031fd0cf8a211421dd715c6553222ed
Megan Fontenot <meganfontenot517@yahoo.com>
2020-06-09 13:52:35 -0500

    Update JadenCasingStrings.js

--- CurrentWork/JadenCasingStrings.js
+++ CurrentWork/JadenCasingStrings.js
@@ -8,5 +8,5 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 String.prototype.toJadenCase = function () {
-  //...
+  //
 };
